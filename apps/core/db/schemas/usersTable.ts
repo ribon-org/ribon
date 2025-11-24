2619874e913e2822246996da1ca73e4a4b2c6142
nikawa2161 <nikawa.2161.t@gmail.com>
2025-11-24 16:00:23 +0900

    ♻️ Userテーブルを追加

--- apps/core/db/schemas/usersTable.ts
+++ apps/core/db/schemas/usersTable.ts
@@ -1,10 +1,16 @@
-import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { timestamp, pgTable, uuid } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
 
 export const usersTable = pgTable("users_table", {
-  id: serial("id").primaryKey(),
-  name: text("name").notNull(),
-  age: integer("age").notNull(),
-  email: text("email").notNull().unique(),
+  id: uuid("id")
+    .default(sql`gen_random_uuid()`)
+    .primaryKey(),
+  supabaseAuthId: uuid("supabase_auth_id").unique().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+  deletedAt: timestamp("deleted_at"),
 });
 
 export type InsertUser = typeof usersTable.$inferInsert;
