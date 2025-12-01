15ce56d0aa405d80de101f64d0b6851317da064b
nikawa2161 <nikawa.2161.t@gmail.com>
2025-12-01 22:43:38 +0900

    🗑️ Removed PUT, DELETE, and PATCH methods

--- apps/core/src/app/api/[[...route]]/route.ts
+++ apps/core/src/app/api/[[...route]]/route.ts
@@ -3,6 +3,3 @@ import app from "@/routes";
 
 export const GET = handle(app);
 export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
-export const PATCH = handle(app);
